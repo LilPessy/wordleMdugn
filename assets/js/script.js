ebffb0bf8c3e2540c9ad7e102bdd2665150fa49a
LilPessy <pettidaniele1f@gmail.com>
2022-02-11 13:56:50 +0100

    Aggiornato script
    le funzioni basilari (molto basilari) sono funzionanti

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 let index = 0;
+let row = 0;
 let word = [];
+let parolaDelGiorno = "tappo";
+let currentRow = document.querySelectorAll(".row")[row];
 
 function fill(value){
     word[index] = value;
     console.log(word);
-    let currentRow = document.querySelectorAll(".row")[0];
     currentRow.children[index].querySelector(".letter").innerHTML = value;
     index++;
 }
@@ -12,19 +14,57 @@ function fill(value){
 function remove(){
     word.pop();
     index--;
-    let currentRow = document.querySelectorAll(".row")[0];
     currentRow.children[index].querySelector(".letter").innerHTML = "";
 }
 
 function checkWord(){
-    console.log(word);
+    let correctLetter = 0;
+    let submittedWord = "";
+
+    for(let i=0; i<word.length; i++){
+        submittedWord += word[i];
+    }
+
+    submittedWord.toLowerCase();
+
+    for(let i=0; i<submittedWord.length; i++){
+        if(parolaDelGiorno.includes(submittedWord[i], 0)){
+            currentRow.children[i].querySelector(".letter").classList.add("presentLetter");
+        }
+    }
+
+    for(let i=0; i<submittedWord.length; i++){
+        if(submittedWord[i]===parolaDelGiorno[i]){
+            currentRow.children[i].querySelector(".letter").classList.remove("presentLetter");
+            currentRow.children[i].querySelector(".letter").classList.add("correctLetter");
+            correctLetter++;
+        }
+    }
+
+    if(correctLetter===5){
+        console.log("hai trovato la parola")
+        return false;
+    }
+
+    return true;
 }
+
+
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+  }
 
 document.addEventListener("keydown", function(e){
 
+
     if(e.key === "Enter" && index === 5){
-        checkWord();
-        console.log(":)")
+        if(checkWord()){
+            if(row<5){
+                index = 0;
+                row++;
+                currentRow = document.querySelectorAll(".row")[row];
+            }
+        }
     }
 
     if(e.key === "Backspace"){
@@ -33,7 +73,7 @@ document.addEventListener("keydown", function(e){
         }
     }
 
-    if(index<5 && e.key!="Backspace" && e.key!="Enter"){
+    if(index<5 && isLetter(e.key)){
         fill(e.key);
     }
 })
