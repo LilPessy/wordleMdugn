355457f9c786bfe7d9583755d5621e60c35ae804
LilPessy <pettidaniele1f@gmail.com>
2022-03-11 16:43:59 +0100

    gigaaggiornamenti dello script
    la tastiera viene evidenziata correttamente. introdotta la dark e lightmode switchabile tramite l'icona in alto a sinistra (da fixare alcuni colori)

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,14 @@ let word = [];
 let parolaDelGiorno = "minni";
 let currentRow = document.querySelectorAll(".row")[row];
 let key = document.querySelectorAll(".keyboardChar");
-console.log(key);
 let modal = document.querySelector(".modal");
-let win = false;
+let end = false;
+var r = document.querySelector(':root');
+
+let themeBtn = document.querySelectorAll(".themeBtn");
 
 function fill(value) {
     word[index] = value;
-    console.log(word);
     currentRow.children[index].querySelector(".letter").innerHTML = value;
     currentRow.children[index].querySelector(".letter").classList.add("filledBorder");
     index++;
@@ -49,14 +50,36 @@ function checkWord() {
 
     for (let i = 0; i < submittedWord.length; i++) {
         currentRow.children[i].querySelector(".letter").classList.add("absentLetter");
-        
-        
+        let j = 0;
+        let finded = false;
+        while(!finded){
+            console.log(key[j].innerHTML.trim().toLowerCase()==submittedWord[i])
+            if(key[j].innerHTML.trim().toLowerCase()==submittedWord[i]){
+                key[j].classList.add("absentLetter");
+                key[j].classList.remove("keyBg");
+                finded = true;
+            }
+            j++;
+        }
     }
 
     for (let i = 0; i < submittedWord.length; i++) {
         if (parolaDelGiorno.includes(submittedWord[i], 0)) {
             currentRow.children[i].querySelector(".letter").classList.remove("absentLetter");
             currentRow.children[i].querySelector(".letter").classList.add("presentLetter");
+            let j = 0;
+            let finded = false;
+            while(!finded){
+                console.log(key[j].innerHTML.trim().toLowerCase()==submittedWord[i])
+                if(key[j].innerHTML.trim().toLowerCase()==submittedWord[i]){
+                    key[j].classList.add("presentLetter");
+                    key[j].classList.remove("absentLetter");
+                    key[j].classList.remove("keyBg");
+                    finded = true;
+                }
+                j++;
+            }
+
             for(let j=i; j>0; j--){
                 if(submittedWord[j]===submittedWord[i] && j!=i){
                     currentRow.children[i].querySelector(".letter").classList.add("absentLetter");
@@ -81,12 +104,31 @@ function checkWord() {
             currentRow.children[i].querySelector(".letter").classList.remove("absentLetter");
             currentRow.children[i].querySelector(".letter").classList.add("correctLetter");
             correctLetter++;
+            let j = 0;
+            let finded = false;
+            while(!finded){
+                console.log(key[j].innerHTML.trim().toLowerCase()==submittedWord[i])
+                if(key[j].innerHTML.trim().toLowerCase()==submittedWord[i]){
+                    key[j].classList.add("correctLetter");
+                    key[j].classList.remove("presentLetter");
+                    key[j].classList.remove("absentLetter");
+                    key[j].classList.remove("keyBg");
+                    finded = true;
+                }
+                j++;
+            }
         }
     }
 
     if (correctLetter === 5) {
         modalMsg("Hai vinto!");
-        win = true;
+        end = true;
+        return false;
+    }
+
+    if (row == 5 && !end) {
+        modalMsg("Hai perso!");
+        end = true;
         return false;
     }
 
@@ -103,7 +145,7 @@ document.addEventListener("keydown", function (e) {
 
     let letter = e.key;
 
-    if (letter === "Enter" && !win) {
+    if (letter === "Enter" && !end) {
         if (checkWord()) {
             if (row < 5) {
                 word.splice(0, word.length);
@@ -114,13 +156,13 @@ document.addEventListener("keydown", function (e) {
         }
     }
 
-    if (letter === "Backspace" && !win) {
+    if (letter === "Backspace" && !end) {
         if (index > 0) {
             remove();
         }
     }
 
-    if (index < 5 && isLetter(letter) && !win) {
+    if (index < 5 && isLetter(letter) && !end) {
         fill(letter);
     }
 })
@@ -130,7 +172,7 @@ for (let i = 0; i < key.length; i++) {
         let letter = e.target.innerHTML;
         letter = letter.trim();
 
-        if (letter === "ENTER" && !win) {
+        if (letter === "ENTER" && !end) {
             if (checkWord()) {
                 if (row < 5) {
                     index = 0;
@@ -140,12 +182,37 @@ for (let i = 0; i < key.length; i++) {
             }
         }
 
-        if (i === 27 && index > 0 && !win) {
+        if (i === 27 && index > 0 && !end) {
             remove();
         }
 
-        if (index < 5 && i != 27 && i != 19 && !win) {
+        if (index < 5 && i != 27 && i != 19 && !end) {
             fill(letter);
         }
     })
 }
+
+for(let i=0; i<themeBtn.length; i++){
+    themeBtn[i].addEventListener("click", function(e){
+        if(i==0){
+           themeBtn[i].classList.add("hidden");
+           themeBtn[i+1].classList.remove("hidden");
+           r.style.setProperty("--background", "white");
+           r.style.setProperty("--letters", "#000");
+           r.style.setProperty("--absentLetter", "#787c7e");
+           r.style.setProperty("--key", "#d3d6da");
+           r.style.setProperty("--letterBorder", "#d3d6da");
+           r.style.setProperty("--filledBorder", "#d3d6da");
+        }else{
+            themeBtn[i].classList.add("hidden");
+            themeBtn[i-1].classList.remove("hidden");
+            r.style.setProperty("--background", "#121213");
+            r.style.setProperty("--letters", "#fff");
+            r.style.setProperty("--absentLetter", "#3a3a3c");
+            r.style.setProperty("--key", "#818384");
+            r.style.setProperty("--letterBorder", "#3a3a3c");
+            r.style.setProperty("--filledBorder", "#565758");
+        }
+    })
+}
+
